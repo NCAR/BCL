2e5c634ddd0e1eb3a00e822aa3c803add2590d6c
jtillots <jtillots@ucar.edu>
2017-02-13 16:16:37 +0000

    use env to get log level
    git-svn-id: https://superion.ucar.edu/svn/cluster/gladenfs/nwsc2-bin@7568 72657bb7-043b-4c92-8c9f-b32df0f6ab50

--- python_modules/nlog.py
+++ python_modules/nlog.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python
 import sys
 import syslog
+import os
 
 def vlog(level, string):
-    #global VERBOSE
-    #if VERBOSE >= level:
-    print string
+    vlevel = 3
+    if 'VERBOSE' in os.environ:
+	vlevel = int(os.environ['VERBOSE'])
+
+    if vlevel >= level:
+	print string
+
     if level < 3:
 	syslog.syslog(string)
 
