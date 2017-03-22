699de1644e12049f580f1d476742369c12864637
jtillots <jtillots@ucar.edu>
2017-03-22 20:21:53 +0000

    copy write_file from ib_diag.py
    git-svn-id: https://superion.ucar.edu/svn/cluster/gladenfs/nwsc2-bin@7731 72657bb7-043b-4c92-8c9f-b32df0f6ab50

--- python_modules/nfile.py
+++ python_modules/nfile.py
@@ -10,3 +10,10 @@ def read_file_first_line(filename):
 	f.close()
     return result
 	
+def write_file ( file_name, contents ):
+    """ Takes file_name and writes contents to file. it will clobber file_name. """
+    vlog(4, 'Writing File: %s SIZE=%s' % (file_name, len(contents)))
+    file = open(file_name, 'w')
+    file.write(contents)
+    file.close()
+
