9c7c171877f02347d683d5366f713784d139f89d
jtillots <jtillots@ucar.edu>
2017-04-07 15:03:30 +0000

    copied and cleaned up some functions from ib_diag.py
    git-svn-id: https://superion.ucar.edu/svn/cluster/gladenfs/nwsc2-bin@7812 72657bb7-043b-4c92-8c9f-b32df0f6ab50

--- python_modules/nfile.py
+++ python_modules/nfile.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 from nlog import vlog
+import subprocess 
 
 def read_file_first_line(filename):
     """ Read first line of given filename """
@@ -13,7 +14,39 @@ def read_file_first_line(filename):
 def write_file ( file_name, contents ):
     """ Takes file_name and writes contents to file. it will clobber file_name. """
     vlog(4, 'Writing File: %s SIZE=%s' % (file_name, len(contents)))
-    file = open(file_name, 'w')
-    file.write(contents)
-    file.close()
+    with open(file_name, 'w') as file:
+	file.write(contents)
+
+def exec_to_file ( cmd, output_file ):
+    """ Runs cmd and pipes STDOUT to output_file """
+    with open(output_file, 'w') as fo:
+	vlog(4, 'Running command: %s > %s '% (cmd, output_file))
+	with subprocess.Popen(cmd, stdout=fo, cwd='/tmp/') as p:
+	    p.wait()
+	    fo.flush()
+	    fo.close()
+	    return p.returncode
+
+    vlog(1, 'Failed to run command: %s > %s '% (cmd, output_file))
+    return None
+
+def exec_to_string_with_input ( cmd, input):
+    """ Runs cmd, sends input to STDIN and places Return Value, STDOUT, STDERR into returned list  """
+    vlog(4, 'Running command: %s' % cmd) 
+    try:
+	with subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd='/tmp/') as p:
+	    stdout, stderr = p.communicate(input=input)
+	    return [p.returncode, stdout, stderr ]
+    except:
+	vlog(1, 'Command %s failed' % cmd)
+	return [-1, '', 'Failed to run']
+
+def exec_to_string ( cmd, cwd='/tmp/' ):
+    """ Runs cmd and places Return Value, STDOUT, STDERR into returned list  """
+    vlog(4, 'Running command: %s' % cmd) 
+    with subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cwd) as p:
+	stdout, stderr = p.communicate()
+	return [ p.returncode, stdout, stderr ] 
+
+    vlog(1, 'Command %s failed' % cmd)
 
