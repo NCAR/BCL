7ea4c8c9be0df0e94dadb68c4454783219994d7e
jtillots <jtillots@ucar.edu>
2017-04-26 23:16:38 +0000

    wrote parser and label generator
    git-svn-id: https://superion.ucar.edu/svn/cluster/gladenfs/nwsc2-bin@8008 72657bb7-043b-4c92-8c9f-b32df0f6ab50

--- python_modules/sgi_cluster.py
+++ python_modules/sgi_cluster.py
@@ -5,6 +5,7 @@
 #
 import socket
 import re
+from nlog import vlog,die_now
 
 def get_cluster_name():
     if re.search("^la", socket.gethostname()):
@@ -80,4 +81,241 @@ def get_ib_speed():
     elif host == 'chmgt':
 	return {'speed': 'EDR', 'link': 25, 'width': '4x'};
     return None
+
+def logical_to_physical(rack, iru):
+    """ Convert SGI logical labels to physical labels """
+    if iru > 4:
+	rack *= 2
+	iru -= 4
+    else: #IRU in rack
+	rack = rack * 2 + 1
+    
+    return {
+	'rack': rack,
+	'iru': iru
+    }
+
+def physical_to_logical(rack, iru):
+    """ Convert SGI physical labels to logical labels """
+    if rack & 1:
+	#odd rack
+	rack = (rack + 1) / 2
+	iru += 4
+    else: #even rack
+	rack /= 2
+
+    return {
+	'rack': rack,
+	'iru': iru
+    }
+
+
+def print_label(v, pformat = None):
+    """ prints an sgi label from dict
+
+    formats:
+	raw: values as dict
+	ibcv2: SGI ibcv2 format (r10i2s0c1.20)
+	firmware: firmare label (r1i0s0 SW1 SwitchX -  Mellanox Technologies)
+	physical: physical label (001IRU2-0-1-14)
+	simple: simple label (r1i0s0 SW1/P1 or r1i3n17)
+    
+    """
+
+    if pformat == 'raw':
+	return str(v)
+    elif pformat == 'ibcv2':
+	return 'r%si%ss%sc%s.%s' % (
+	    v['rack'],
+	    v['iru'],
+	    v['switch'],
+	    v['switch_chip'],
+	    v['port']
+	)
+    elif pformat == 'firmware':
+	if not v['switch'] is None:
+	    if v['port'] is None:
+		return 'r%si%ss%s SW%s SwitchX -  Mellanox Technologies' % (
+		    v['rack'],
+		    v['iru'],
+		    v['switch'],
+		    v['switch_chip']
+		)
+	    else:
+ 		return 'r%si%ss%s SW%s SwitchX -  Mellanox Technologies/P%s' % (
+		    v['rack'],
+		    v['iru'],
+		    v['switch'],
+		    v['switch_chip'],
+		    v['port']
+		)
+	elif not v['node'] is None:
+ 	    return 'r%si%ss%s/U%s/P%s' % (
+		v['rack'],
+		v['iru'],
+		v['node'],
+		v['hca'] if v['hca'] else 1, #default to first hca
+		v['port'] if v['port'] else 1, #default to first port
+	    ) 
+    elif pformat == 'physical':
+	if not v['port'] is None:
+	    return '{0:0>3}IRU{1}-{2}-{3}-{4}'.format(
+		v['rack'],
+		v['iru'],
+		v['switch'],
+		v['switch_chip'],
+		v['port']
+	    )
+	else:
+	    return '{0:0>3}IRU{1}-{2}-{3}'.format(
+		v['rack'],
+		v['iru'],
+		v['switch'],
+		v['switch_chip']
+	    ) 
+    elif pformat == 'simple':
+	if not v['node'] is None:
+	    return 'r%si%sn%s' % (
+		v['rack'],
+		v['iru'],
+		v['node']
+	    ) 
+	if not v['switch'] is None:
+	    if not v['port'] is None:
+		return 'r%si%ss%s SW%s/P%s' % (
+		    v['rack'],
+		    v['iru'],
+		    v['switch'],
+		    v['switch_chip'],
+		    v['port']
+		)  
+	    else:
+		return 'r%si%ss%s SW%s' % (
+		    v['rack'],
+		    v['iru'],
+		    v['switch'],
+		    v['switch_chip']
+		) 
+    vlog(1, 'unknown format: %s' % (pformat))	    
+    return ''
+
+def parse_label(label):
+    """ Parse the sgi label names """
+
+    def tint(v):
+	if v:
+	    return int(v)
+	else:
+	    return None
+
+    vlog(5, 'parse_label(%s)' % (label))
+
+    v = {
+	'rack': None,
+	'iru': None,
+	'switch': None,
+	'switch_chip': None,
+	'node': None,
+	'port': None,
+	'hca': None,
+    }
+
+    #SGI ibcv2 format
+    #r1i0s0c0.16
+    #r9i2s0c1.20
+    #r10i2s0c1.20
+    r1 = re.compile(
+	r"""
+	r(?P<rack>[0-9]+)  #E-cell Rack - not E-Cell number
+	i(?P<iru>[0-9]+)
+	s(?P<switch>[0-9]+)
+	c(?P<swchip>[0-9]+)
+	\.
+	(?P<port>[0-9]+)
+	\s*
+	""",
+	re.VERBOSE
+	)
+    #r1i3n17/U1/P1
+    r2 = re.compile(
+	r"""
+	r(?P<rack>[0-9]+)  #E-cell Rack - not E-Cell number
+	i(?P<iru>[0-9]+)
+	n(?P<node>[0-9]+)
+	(?:
+	    (?:/U(?P<hca>\d+)|)
+	    (?:/P(?P<port>\d+)|)
+	)
+	\s*
+	""",
+	re.VERBOSE
+	)
+    #r1i0s0 SW1 SwitchX -  Mellanox Technologies
+    #r1i0s0 SW0/P27
+    r3 = re.compile(
+	r"""
+	r(?P<rack>[0-9]+)  #E-cell Rack - not E-Cell number
+	i(?P<iru>[0-9]+)
+	s(?P<switch>[0-9]+)
+ 	(?: 
+	    \s+SW(?P<swchip>\d+)|
+	) 
+ 	(?:
+	    \s+SwitchX\s+\-+\s+Mellanox\ Technologies\s*|
+	) 
+ 	(?:
+	    /P(?P<port>\d+)|
+	) 
+	\s*
+	""",
+	re.VERBOSE
+	)     
+    #001IRU2-0-1-14 
+    r4 = re.compile(
+	r"""
+	(?P<rack>[0-9]+)  #E-cell Rack - not E-Cell number
+	IRU(?P<iru>[0-9]+)
+	(?P<switch>[0-9]+)
+	-
+	(?P<swchip>\d+)
+	-
+	(?P<port>\d+)
+	\s*
+	""",
+	re.VERBOSE
+	)     
  
+    match = r1.match(label) 
+    if match:        
+	v['rack'] = tint(match.group('rack'))
+	v['iru'] = tint(match.group('iru'))
+	v['switch'] = tint(match.group('switch'))
+	v['switch_chip'] = tint(match.group('swchip'))
+	v['port'] = tint(match.group('port'))
+	return v
+    match = r2.match(label) 
+    if match:        
+	v['rack'] = tint(match.group('rack'))
+	v['iru'] = tint(match.group('iru'))
+	v['node'] = tint(match.group('node'))
+	v['port'] = tint(match.group('port'))
+	v['hca'] = tint(match.group('hca'))
+	return v
+    match = r3.match(label) 
+    if match:        
+	v['rack'] = tint(match.group('rack'))
+	v['iru'] = tint(match.group('iru'))
+	v['switch'] = tint(match.group('switch'))
+	v['switch_chip'] = tint(match.group('swchip'))
+	v['port'] = tint(match.group('port'))
+	return v    
+    match = r4.match(label) 
+    if match:        
+	v['rack'] = tint(match.group('rack'))
+	v['iru'] = tint(match.group('iru'))
+	v['switch'] = tint(match.group('switch'))
+	v['switch_chip'] = tint(match.group('swchip'))
+	v['port'] = tint(match.group('port'))
+	return v    
+    return None
+
